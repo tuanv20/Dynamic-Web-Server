d1a7f0d9457383d35654ce9a1a74d82262d663c0
pcgabor <rebeday124@gmail.com>
2023-05-18 10:09:08 -0400

    Added react home page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*Server-side code using Express to handle request routing and file serving*/
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
@@ -12,9 +13,20 @@ let sendHello = function(res){
 
 const app = express();
 const port = 8080;
+//Specifies folder with static content that server needs to serve (css, images, static files, etc)
 const static_path = path.join(__dirname, "public");
 app.use(express.static(static_path));
 
+//Endpoints 
+app.get("/content/resume.html", (req, res) =>{
+    res.send("This is a resume");
+})
+
+app.get("/content/homepage.html", (req, res) =>{
+    res.sendFile("/content/homepage.html");
+})
+
+//Starts up the server
 app.listen(port,  ()=>{
     console.log(`Server is running on port ${port}`)
  })
