dce37c2e571233614e914bc8faee0905f0e0d2fc
pcgabor <rebeday124@gmail.com>
2023-05-18 09:20:18 -0400

    Implemented '/' GET request for home page and resume

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
 const http = require('http');
+const path = require('path');
 const fs = require('fs');
+const express = require("express");
 
-const server = http.createServer(function(req, res){
+let sendHello = function(res){
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.write("Hello World");
     res.end();
-    }
-)
+}
 
+const app = express();
 const port = 8080;
+const static_path = path.join(__dirname, "public");
+app.use(express.static(static_path));
 
-server.listen(port, function(){
-    console.log("Server Running on Port: " + port);
-})
+app.listen(port,  ()=>{
+    console.log(`Server is running on port ${port}`)
+ })
